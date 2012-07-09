d7e0623353d9d65131215a5e7f1a5c015e9b8fe4
Joel Berger <joel.a.berger@gmail.com>
2012-07-09 14:52:36 -0500

    removed incorrect line

--- lib/MojoCMS/DB/Schema/Result/Page.pm
+++ lib/MojoCMS/DB/Schema/Result/Page.pm
@@ -4,7 +4,6 @@ use base qw/DBIx::Class::Core/;
 __PACKAGE__->table('pages');
 __PACKAGE__->add_columns( qw/ pageid name / );
 __PACKAGE__->set_primary_key('pageid');
-__PACKAGE__->has_many( pages => 'MyDB::Schema::Result::Page' );
 __PACKAGE__->belongs_to( user => 'MyDB::Schema::User', 'userid' );
 
 1;
