e6a772a605ae62683bb48480689f0cb73cced950
Joel Berger <joel.a.berger@gmail.com>
2012-07-14 15:57:53 -0500

    prompt for username and password for admin account

--- lib/Mojolicious/Command/create_database.pm
+++ lib/Mojolicious/Command/create_database.pm
@@ -1,6 +1,8 @@
 package Mojolicious::Command::create_database;
 use Mojo::Base 'Mojolicious::Command';
 
+use Term::Prompt qw/prompt/;
+
 has description => "Create the database for your MojoCMS application.\n";
 has usage       => "usage: $0 create_database [username]\n";
 
@@ -8,15 +10,27 @@ use Mojo::JSON;
 my $json = Mojo::JSON->new();
 
 sub run {
-  my ($self, $user) = @_;
+  my ($self) = @_;
+
+  my $user = prompt('x', 'Admin Username: ', '', 'admin');
+  my $pass1 = prompt('p', 'Admin Password: ', '', '');
+  print "\n";
+
+  #TODO check for acceptable password
+
+  my $pass2 = prompt('p', 'Repeat Admin Password: ', '', '');
+  print "\n";
+
+  unless ($pass1 eq $pass2) {
+    die "Passwords do not match";
+  }
 
   my $schema = $self->app->db_connect;
-
   $schema->deploy;
 
   my $admin = $schema->resultset('User')->create({
-    name => $user || 'admin',
-    pass => 'pass',
+    name => $user,
+    pass => $pass1,
     is_author => 1,
     is_admin  => 1,
   });
