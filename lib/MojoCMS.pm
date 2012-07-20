9194cfb1c53ee4adf07522843b8007335a06324f
Joel Berger <joel.a.berger@gmail.com>
2012-07-20 13:30:27 -0500

    move the user menu logic to a template

--- lib/MojoCMS.pm
+++ lib/MojoCMS.pm
@@ -18,52 +18,8 @@ sub startup {
 
   $app->secret( $app->config->{secret} );
 
-  my $schema = do {
-    my $schema_class = $app->config->{db_schema} or die "Unknown DB Schema Class";
-    eval "require $schema_class" or die "Could not load Schema Class ($schema_class)";
-
-    my $db_connect = $app->config->{db_connect} or die "No DBI connection string provided";
-    my $schema = $schema_class->connect( $db_connect ) 
-      or die "Could not connect to $schema_class using $db_connect";
-
-    $schema;
-  };
-
+  my $schema = $app->db_connect;
   $app->helper( schema => sub { return $schema } );
-
-  $app->helper( user_menu => sub {
-    my $self = shift;
-    my $user = $self->session->{username};
-
-    my $url = $self->tx->req->url;
-    my $html;
-    if ($user) {
-      my $edit_this_page = 
-        $url =~ s{/pages/}{/edit/} 
-        ? qq{<li><a href="$url">Edit This Page</a></li>} 
-        : '';
-      $html = <<USER;
-<div class="well" style="padding: 8px 0;">
-  <ul class="nav nav-list">
-    <li class="nav-header">Hello $user</li>
-    $edit_this_page
-    <li><a href="/admin/menu">Setup Nav Menu</a></li>
-    <li><a href="/logout">Log Out</a></li>
-  </ul>
-</div>
-USER
-    } else {
-      $html = <<ANON;
-<form class="well" method="post" action="/login">
-  <input type="hidden" name="from" value="$url">
-  <input type="text" class="input-small" placeholder="Username" name="username">
-  <input type="password" class="input-small" placeholder="Password" name="password">
-  <input type="submit" class="btn" value="Sign In">
-</form>
-ANON
-    }
-    return Mojo::ByteStream->new( $html );
-  });
 
   $app->helper( 'get_menu' => sub {
     my $self = shift;
@@ -100,6 +56,18 @@ ANON
   $if_author->any( '/admin/menu' )->to('editor#edit_menu');
   $if_author->any( '/edit/:name' )->to('editor#edit_page');
   $if_author->websocket( '/store' )->to('editor#ws_update');
+}
+
+sub db_connect {
+  my $self = shift;
+  my $schema_class = $self->config->{db_schema} or die "Unknown DB Schema Class";
+  eval "require $schema_class" or die "Could not load Schema Class ($schema_class)";
+
+  my $db_connect = $self->config->{db_connect} or die "No DBI connection string provided";
+  my $schema = $schema_class->connect( $db_connect ) 
+    or die "Could not connect to $schema_class using $db_connect";
+
+  return $schema;
 }
 
 1;
