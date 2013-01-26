6d90b33d63d2a2c47a0296274912fb9e1444bd38
Joel Berger <joel.a.berger@gmail.com>
2013-01-26 15:06:12 -0600

    added title handling to `galileo dump`

--- lib/Galileo/Command/dump.pm
+++ lib/Galileo/Command/dump.pm
@@ -2,26 +2,48 @@ package Galileo::Command::dump;
 use Mojo::Base 'Mojolicious::Command';
 use File::Spec;
 use Mojo::Util 'spurt';
+use Getopt::Long qw/GetOptionsFromArray/;
 
 has description => "Dump all stored pages as markdown\n";
 has usage => <<END;
-usage: $0 dump [directory]
+usage: $0 dump [options]
+options: 
 
-An optional directory to dump to may be specified; the directory 
-must exist. By default it dumps to the current working directory.
+--directory,-d
+  An optional directory to dump to may be specified; the directory 
+  must exist. By default it dumps to the current working directory.
+
+--title,-t
+  By default the title of the page is included as an HTML comment.
+  This option accepts an sprintf format for including the title.
+  As a special case, if this flag is given without argument, an h1 
+  title is created.
 END
 
 sub run {
   my $self = shift;
-  my $dir = shift || '.';
-  die "$dir does not exist" unless -d $dir;
 
-  my $db = $self->app->schema;
-  my $pages = $db->resultset('Page')->search;
-  while ( my $page = $pages->next ) {
-    my $file = File::Spec->catfile( $dir, $page->name . '.md' );
-    spurt $page->md, $file;
+  GetOptionsFromArray( \@_,
+    'directory|d=s' => \my $dir,
+    'title|t:s'     => \(my $title = '<!-- %s -->'),
+  );
+
+  $title = '# %s' unless $title;
+  if ( $dir and ! -d $dir ) {
+    die qq{Directory "$dir" does not exist\n};
   }
+
+  my $pages = $self->app->schema->resultset('Page')->search;
+  while ( my $page = $pages->next ) {
+    my $file = $page->name . '.md';
+    $file = File::Spec->catfile( $dir, $file ) if $dir;
+
+    my $content = sprintf "$title\n", $page->title;
+    $content .= $page->md;
+    spurt $content, $file;
+  }
+
+  say "Export Complete";
 }
 
 1;
