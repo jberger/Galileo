1a531a11cec95f57dce809edf5c5d0c8456a2e77
Joel Berger <joel.a.berger@gmail.com>
2013-03-04 11:59:55 -0600

    web setup is now the only setup

--- lib/Galileo/Command/setup.pm
+++ lib/Galileo/Command/setup.pm
@@ -1,151 +1,296 @@
 package Galileo::Command::setup;
 use Mojo::Base 'Mojolicious::Command';
 
-use Term::Prompt qw/prompt/;
+use Mojolicious::Command::daemon;
 
-has description => "Deploy/upgrade the database for your Galileo CMS application.\n";
-has usage       => "usage: $0 setup\n";
+use Mojolicious::Routes;
+use Mojo::JSON 'j';
+use Mojo::Util 'spurt';
 
 use Galileo::DB::Deploy;
 
+has description => "Configure your Galileo CMS via a web interface\n";
+
 sub run {
-  my ($self) = @_;
+  my ($self, @args) = @_;
 
-  print <<'END';
+  my $app = $self->app;
 
-#############
-#  Welcome  #
-#############
+  my $r = Mojolicious::Routes->new;
+  $app->routes($r); # remove all routes
 
-This is Galileo's database installation and upgrade tool.
+  push @{ $app->renderer->classes }, __PACKAGE__;
 
-By default it will install a new SQLite database called 'galileo.db' in your current
-working directory. You may change these behaviors using the following 
-environment variables:
+  $app->helper( dh => sub {
+    my $self = shift;
+    state $dh = Galileo::DB::Deploy->new( schema => $self->app->schema );
+    $dh;
+  });
 
-  GALILEO_HOME 
+  $app->helper( 'control_group' => sub {
+    my $self = shift;
+    my $contents = pop;
+    my %args = @_;
+ 
+    $self->render(
+      partial => 1,
+      template => 'control_group',
+      'control_group.contents' => ref $contents ? $contents->() : $contents,
+      'control_group.label' => $args{label} || '',
+      'control_group.for'   => $args{for}   || '',
+    );
+  });
 
-    The full path to a directory which will contain any configuration
-    files, the SQLite database (if applicable) and any static content.
-    The default is the current working directory.
+  $r->any( '/' => 'galileo_setup' );
+  $r->any( '/configure' => 'galileo_config' );
+  $r->any( '/store_config' => sub {
+    my $self = shift;
+    my @params = sort $self->param;
 
-  GALILEO_CONFIG
+    {
+      my @config = sort keys %{ $self->app->config };
 
-    The name of the configuration file controlling the rest of Galileo's
-    functionality. The default is 'galileo.conf'.
+      # check that all keys are represented
+      die "Incorrect number of configuration keys"
+        unless @params == @config;
 
-The remaining behaviors, including the database connection can be controlled by using 
-this configuration file. To create this file, please abort this script and run 
-`galileo config` first, then edit the file it creates.
+      for my $i (0 .. $#params) {
+        die "Config key mismatch $config[$i] vs $params[$i]"
+          unless $config[$i] eq $params[$i];
+      }
+    }
 
-############
-#  Secret  #
-############
+    # map JSON keys to Perl data
+    my %params = map { $_ => scalar $self->param($_) } @params;
+    foreach my $key ( qw/files extra_css extra_js db_options/ ) {
+      $params{$key} = j($params{$key});
+    }
 
-Finally, while you do not NEED a configuration file, one more thing that it does is set
-the "Secret" for you website. This helps to keep your site secure, you really should
-set it. Mojolicious will warn you until you do.
+    {
+      require Data::Dumper;
+      local $Data::Dumper::Terse = 1;
+      local $Data::Dumper::Sortkeys = 1;
 
-END
+      spurt Data::Dumper::Dumper(\%params), $self->app->config_file;
+    }
+  
+    $self->app->load_config;
+    $self->humane_flash( 'Configuration saved' );
+    $self->redirect_to('/');
+  });
 
-print '  FYI, your secret is ...... ' . ( $self->app->config->{secret} ? 'SET' : 'NOT SET' ) . "\n";
+  $r->any( '/database' => sub {
+    my $self = shift;
 
-print <<'END';
+    my $dh = $self->dh;
+    my $schema = $dh->schema;
 
-#################
-#  Please Note  #
-#################
+    my $available = $schema->schema_version;
 
-Unfortunately the database migration tools sometimes spit out warnings like
+    # Nothing installed
+    unless ( $dh->has_admin_user ) {
+      return $self->render( 'galileo_database_install' );
+    }
 
-  Overwriting existing DDL-YML file - /tmp/JG6_FothpG ... 
+    # Something is installed, check for a version
+    my $installed = $dh->installed_version || $dh->setup_unversioned;
 
-or 
+    # Do nothing if version is current
+    if ( $installed == $available ) {
+      $self->flash( 'galileo.message' => 'Database schema is current' );
+    } else {
+      $self->flash( 'galileo.message' => "Upgrade database $installed -> $available" );
+      $dh->do_upgrade;
+    }
 
-  SV = IV(0x5042fd8) at 0x5042fe8 ...
+    $self->redirect_to('finish');
+  });
 
-These messages do not come from Galileo. They can safely be ignored and bugs have 
-been filed where necessary. Thanks for your understanding.
+  $r->any( '/database_install' => sub {
+    my $self = shift;
+    my $pw1 = $self->param('pw1');
+    my $pw2 = $self->param('pw2');
+    unless ( $pw1 eq $pw2 ) {
+      $self->humane_flash( q{Passwords don't match!} );
+      return $self->redirect_to('database');
+    }
 
-########################
-#  Let's get started!  #
-########################
+    my $dh = $self->dh;
+    my $user = $self->param('user');
+    my $full = $self->param('full');
 
-END
+    $dh->do_install;
+    $dh->inject_sample_data($user, $pw1, $full);
 
-  my $dh = Galileo::DB::Deploy->new( schema => $self->app->schema );
-  $self->deploy_or_upgrade_schema( $dh );
+    $self->flash( 'galileo.message' => 'Database has been setup' );
+    $self->redirect_to('finish');
+  });
 
-  print <<'END';
+  $r->any('/finish' => sub {
+    my $self = shift;
+    my $message = $self->flash( 'galileo.message' );
 
-##############
-#  Complete  #
-##############
+    # check that an admin user exists
+    if ( $self->app->dh->has_admin_user ) {
+      $self->stash( 'galileo.success' => 1 );
+      $self->stash( 'galileo.message' => $message );
+    } else {
+      $self->stash( 'galileo.success' => 0 );
+      $self->stash( 
+        'galileo.message' =>
+        'It does not appear that your database is setup, please rerun the setup utility'
+      );
+    }
 
-All necessary actions have finished.
-Please run 'galileo daemon' to start the server.
+    $self->humane_stash( 'Goodbye' );
+    $self->render('galileo_finish');
+  });
 
+  $r->any('/exit' => sub { exit });
 
-END
-}
-
-sub deploy_or_upgrade_schema {
-  my $self = shift;
-  my $dh = shift;
-  my $schema = $dh->schema;
-
-  my $available = $schema->schema_version;
-
-  # Nothing installed
-  unless ( $dh->has_admin_user ) {
-    say "Installing database version: $available";
-    $self->install_schema( $dh );
-    return;
-  }
-
-  # Something is installed, check for a version
-  my $installed = $dh->installed_version || $dh->setup_unversioned;
-
-  # Do nothing if version is current
-  if ( $installed == $available ) {
-    say "Database schema is current";
-    return;
-  }
-
-  say "Upgrade database $installed -> $available";
-
-  $dh->do_upgrade;
-}
-
-sub install_schema {
-  my $self = shift;
-  my $dh = shift;
-
-  my ($user, $full, $pass) = $self->prompt_for_user_info;
-
-  $dh->do_install;
-  $dh->inject_sample_data($user, $pass, $full);
-}
-
-sub prompt_for_user_info {
-
-  my $self = shift;
-
-  my $user = prompt('x', 'Admin Username: ', '', '');
-  my $full = prompt('x', 'Admin Full Name: ', '', '');
-  my $pass1 = prompt('p', 'Admin Password: ', '', '');
-  print "\n";
-
-  #TODO check for acceptable password
-
-  my $pass2 = prompt('p', 'Repeat Admin Password: ', '', '');
-  print "\n";
-
-  unless ($pass1 eq $pass2) {
-    die "Passwords do not match";
-  }
-
-  return ( $user, $full, $pass1 );
+  $self->Mojolicious::Command::daemon::run(@args);
 }
 
 1;
+
+
+__DATA__
+
+@@ galileo_setup.html.ep
+
+% title 'Galileo Setup - Home';
+% layout 'basic';
+
+<p>Welcome to Galileo! This utility helps you setup your Galileo CMS.</p>
+
+<ul>
+  %= tag li => begin 
+    %= link_to 'Configure your Galileo CMS' => 'configure'
+    <p>Configuration is not necessary, defaults can be used. 
+    Configuring Galileo CMS should be done before installing the database.</p>
+  % end
+
+  %= tag li => begin
+    %= link_to 'Install or upgrade your database' => 'database'
+    <p>If this is a new installation you <b>must</b> to run the database setup utility.
+    If you have not configured Galileo (see above), you will use the defaults, including using an SQLite database for the backend.</p>
+  % end
+
+  %= tag li => begin
+    %= link_to 'Stop and exit' => 'finish'
+    <p>If your database is already installed, you may stop this utility and run <pre>$ galileo daemon</pre></p>
+  % end
+
+</ul>
+
+@@ galileo_config.html.ep
+
+% use Mojo::JSON 'j';
+% title 'Galileo Setup - Configure';
+% layout 'basic';
+
+%= form_for 'store_config' => method => 'POST', class => 'form-horizontal' => begin
+  % my $config = app->config;
+
+  <legend>Database Connection</legend>
+  %= control_group for => 'db_dsn', label => 'Connection String (DSN)' => begin
+    %= text_field 'db_dsn', value => $config->{db_dsn}
+  % end
+  %= control_group for => 'db_username', label => 'Username' => begin
+    %= text_field 'db_username', value => $config->{db_username}
+  % end
+  %= control_group for => 'db_password', label => 'Password' => begin
+    %= input_tag 'db_password', value => $config->{db_password}, type => 'password'
+  % end
+  %= control_group for => 'db_options', label => 'Options (JSON hash)' => begin
+    %= text_field 'db_options', value => j($config->{db_options})
+  % end
+  %= control_group for => 'db_schema', label => 'Schema Class' => begin
+    %= text_field 'db_schema', value => $config->{db_schema}
+  % end
+
+  <legend>Additional Files</legend>
+
+  %= control_group for => 'files', label => 'Static Files (JSON array)' => begin
+    %= text_field 'files', value => j($config->{files})
+  % end
+  %= control_group for => 'extra_js', label => 'Extra Javascript Files (JSON array)' => begin
+    %= text_field 'extra_js', value => j($config->{extra_js})
+  % end
+  %= control_group for => 'extra_css', label => 'Extra Stylesheet files (JSON array)' => begin
+    %= text_field 'extra_css', value => j($config->{extra_css})
+  % end
+
+  <legend>Other Options</legend>
+
+  %= control_group for => 'sanitize', label => 'Use Sanitizing Editor' => begin 
+    %= check_box 'sanitize', value => 1, checked => $config->{sanitize} ? 'checked' : ''
+  % end
+  %= control_group for => 'secret', label => 'Application Secret' => begin
+    %= text_field 'secret', value => $config->{secret}
+  % end
+  %= control_group for => 'submit-button', begin
+    <button class="btn" id="submit-button" type="submit">Save</button>
+    %= link_to 'Cancel' => '/' => class => 'btn'
+  % end
+% end
+
+@@ galileo_database_install.html.ep
+
+% title 'Galileo Setup - Database';
+% layout 'basic';
+
+%= form_for 'database_install' => method => 'POST', class => 'form-horizontal' => begin
+  %= control_group for => 'full', label => 'Admin Full Name' => begin
+    %= text_field 'full'
+  % end
+  %= control_group for => 'user', label => 'Admin Username' => begin
+    %= text_field 'user'
+  % end
+  %= control_group for => 'pw1', label => 'Password' => begin
+    %= input_tag 'pw1', type => 'password'
+  % end
+  %= control_group for => 'pw2', label => 'Password' => begin
+    %= input_tag 'pw2', type => 'password'
+  % end
+
+  %= control_group for => 'submit-button', begin
+    <button class="btn" id="submit-button" type="submit">Save</button>
+    %= link_to 'Cancel' => 'finish' => class => 'btn'
+  % end
+% end
+
+@@ galileo_finish.html.ep
+
+% title 'Galileo Setup - Finished';
+% layout 'basic';
+
+% if ( my $message = stash 'galileo.message' ) {
+  <p><%= $message %></p>
+% }
+
+% if ( stash 'galileo.success' ) {
+  <p>Setup complete, run <pre>$ galileo daemon</pre></p>
+% }
+
+%= javascript begin
+  $(function(){ $.get('<%= url_for 'exit' %>') });
+% end
+
+@@ control_group.html.ep
+
+<div class="control-group">
+  % if (my $label = stash 'control_group.label') {
+    % my @for;
+    % if ( my $for = stash 'control_group.for' ) {
+      % push @for, for => $for;
+    % }
+    %= tag label => class => 'control-label', @for, begin
+      %= $label 
+    % end
+  % }
+ 
+  <div class="controls">
+    %= stash 'control_group.contents'
+  </div>
+</div>
